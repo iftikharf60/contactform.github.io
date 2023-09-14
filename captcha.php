2875010c502e00e8f8a40077f4c0c0a3556b1ebb
IFTIKHAR <109800110+iftikharf60@users.noreply.github.com>
2023-09-14 16:42:36 +0530

    chnages

--- captcha.php
+++ captcha.php
@@ -1,26 +1,61 @@
 <?php
-//Start session
+
 session_start();
-//Generate random number with md5 encryption
-$captcha = md5(rand(1000, 9999));
-//Get 6 digits encrypted main captcha code from a random number
-$captcha_code = substr($captcha, 0, 6);
-//Store captcha in session
-$_SESSION['captcha'] = $captcha_code;
-//Generate standard captcha image
-$captcha_img = imagecreatetruecolor(65, 27); 
-//Background color
-$captcha_bg = imagecolorallocate($captcha_img, 22, 86, 165);
-//Give captcha image a background
-imagefill($captcha_img, 0, 0, $captcha_bg);
-//foreground color
-$captcha_fg = imagecolorallocate($captcha_img, 255, 255, 255);
-//Print captcha text in the image with a random position and size
-imagestring($captcha_img, 7, 7, 5,  $captcha_code, $captcha_fg);
-//Prevent browser cache
-header("Cache-Control: no-store, no-cache, must-revalidate");
-//Render this PHP file as an image
+
+$permitted_chars = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+
+function generate_string($input, $strength = 10) {
+    $input_length = strlen($input);
+    $random_string = '';
+    for($i = 0; $i < $strength; $i++) {
+        $random_character = $input[mt_rand(0, $input_length - 1)];
+        $random_string .= $random_character;
+    }
+
+    return $random_string;
+}
+
+$image = imagecreatetruecolor(200, 50);
+
+imageantialias($image, true);
+
+$colors = [];
+
+$red = rand(125, 175);
+$green = rand(125, 175);
+$blue = rand(125, 175);
+
+for($i = 0; $i < 5; $i++) {
+  $colors[] = imagecolorallocate($image, $red - 20*$i, $green - 20*$i, $blue - 20*$i);
+}
+
+imagefill($image, 0, 0, $colors[0]);
+
+for($i = 0; $i < 10; $i++) {
+  imagesetthickness($image, rand(2, 10));
+  $line_color = $colors[rand(1, 4)];
+  imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $line_color);
+}
+
+$black = imagecolorallocate($image, 0, 0, 0);
+$white = imagecolorallocate($image, 255, 255, 255);
+$textcolors = [$black, $white];
+
+$font = 'arial.ttf';
+
+$string_length = 6;
+$captcha_string = generate_string($permitted_chars, $string_length);
+
+$_SESSION['captcha_text'] = $captcha_string;
+
+for($i = 0; $i < $string_length; $i++) {
+  $letter_space = 170/$string_length;
+  $initial = 15;
+
+  imagettftext($image, 24, rand(-15, 15), $initial + $i*$letter_space, rand(25, 45), $textcolors[rand(0, 1)], $font, $captcha_string[$i]);
+}
+
 header('Content-type: image/png');
-//Show the captcha as a PNG image on the browser
-imagepng($captcha_img);
+imagepng($image);
+imagedestroy($image);
 ?>
--- form.php
+++ form.php
@@ -1,58 +1,129 @@
 <?php
-//start session to match with captcha session variable
-session_start();
-//Get form data
-$captcha = $_POST['captcha'];
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-//validation for email
-$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-if (!preg_match($email_exp, $email)) {
-?>
-<div class="alert alert-success alert-dismissible fade show">
-The Email address you entered is not valid.
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-<?php
-exit;
+// Your Email
+    $recipient = "iftikharbq@gmail.com"; 
+
+
+    // Check $recipient
+    if($recipient === '') {
+        returnAndExitAjaxResponse(
+            constructAjaxResponseArray(
+                FALSE,
+                'RECIPIENT_IS_NOT_SET',
+                array('error_message'=> 'RECIPIENT email address is not set. Please configure the script.')
+            )
+        );
+    }
+
+if ($_POST['captcha_challenge'] == $_SESSION['captcha_text']) {
+
+
+    // Check for empty required field
+    if(!isset($_POST["email"]) || !isset($_POST["fname"]) || !isset($_POST["message"])) {
+        returnAndExitAjaxResponse(
+            constructAjaxResponseArray(
+                FALSE,
+                'MISSING_REQUIRED_FIELDS',
+                array('error_message'=> 'MISSING_REQUIRED_FIELDS should not be occurred.')
+            )
+        );
+    }
+
+    // Sanitize input
+    $fname  = filter_var($_POST["fname"], FILTER_SANITIZE_STRING);
+    $lname  = filter_var($_POST["lname"], FILTER_SANITIZE_EMAIL);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+
+    // Headers
+    $headers = 'From: '.$fname.' <'.$email.'>' . "\r\n";
+    $headers .= 'Reply-To: '.$email.'' . "\r\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+
+    // Subject
+    $subject = "Neue E-Mail von Kontakt Formular";
+
+    // Build Message
+    $email_content = "Firma: $fname\n";
+    $email_content .= "Ihr Name: $lname\n";
+    $email_content .= "Telefon nummer: $telefonnummer\n"; 
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Nachricht:\n$message\n\n\n";
+    $email_content .= "CLIENT IP:\n".get_client_ip()."\n";
+    $email_content .= "HOST IP:\n".$_SERVER['SERVER_ADDR']."\n";
+
+else {
+    } echo '<p>Captcha verification is wrong. Take other action.</p>';
+
 }
-//Match with captcha session data
-if($captcha == $_SESSION['captcha']){
-$to = "iftikharbq@gmail.com";  //recipient email address
-$subject = "Contact Form";  //Subject of the email
-//Message content to send in an email
-$message = "Name: ".$name."<br>Email: ".$email."<br> Message: ".$message;
-// Set content type as HTML
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-//Email headers
-$headers .= "From:".$email."\r\n";
-//$headers .= "CC: someone@example.com";
-$headers .= "Reply-To:".$email."\r\n";
-//Send email 
-$sendmail = mail($to, $subject, $message, $headers);
-if($sendmail == true){ 
-?>
-<div class="alert alert-success alert-dismissible fade show">
-The message has been sent successfully.
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-<?php
-}else {
-?>
-<div class="alert alert-danger alert-dismissible fade show">
-The message could not be sent.
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-<?php
+
+
+// Check if sent
+try {
+    $sendmailResult = mail($recipient, $subject, $email_content, $headers);
+    if( $sendmailResult === TRUE ) {
+        returnAndExitAjaxResponse(
+            constructAjaxResponseArray(
+                TRUE
+            )
+        );
+    } else {
+        returnAndExitAjaxResponse(
+            constructAjaxResponseArray(
+                FALSE,
+                'ERROR_AT_PHPMAIL',
+                array('error_information'=> error_get_last() )
+            )
+        );
+    }
+} catch (Exception $_e) {
+    returnAndExitAjaxResponse(
+        constructAjaxResponseArray(
+            TRUE,
+            'ERROR_AT_PHPMAIL',
+            array('error_message'=> $_e->getMessage())
+        )
+    );
 }
-}else{
-?>
-<div class="alert alert-danger alert-dismissible fade show">
-Captcha is not matching. Please enter the correct captcha code.
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-<?php
+
+
+function constructAjaxResponseArray ($_response, $_message = '', $_json = null) {
+    $_responseArray = array();
+    $_response = ( $_response === TRUE ) ? TRUE : FALSE;
+    $_responseArray['response'] = $_response;
+    if(isset($_message)) $_responseArray['message'] = $_message;
+    if(isset($_json)) $_responseArray['json'] = $_json;
+
+    return $_responseArray;
 }
-?>
+
+function returnAndExitAjaxResponse ($_ajaxResponse) {
+    if(!$_ajaxResponse){
+        $_ajaxResponse = array('response'=>false,'message'=>'Unknown error occurred.');
+    }
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($_ajaxResponse);
+    die();
+}
+
+
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    } else if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else if(isset($_SERVER['HTTP_X_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    } else if(isset($_SERVER['HTTP_FORWARDED_FOR'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    } else if(isset($_SERVER['HTTP_FORWARDED'])) {
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    } else if(isset($_SERVER['REMOTE_ADDR'])) {
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+    return $ipaddress;
+}
